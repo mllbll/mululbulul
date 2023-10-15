55b0dfae574ab989a023117ba85bde05c02f7190
petrucho <zona904@yandex.ru>
2023-10-15 16:21:22 +0300

    authorization bylettr sqlite by me

--- dz_11/01.py
+++ dz_11/01.py
@@ -1,30 +1,15 @@
 from PIL import Image, ImageDraw, ImageFilter, ImageEnhance
-import matplotlib.pyplot as plt
 import requests
 import sys
-import glob
-
-# url = 'ссылка на фото'
-url='Papa_smurf.jpg'
-
-try:
-    resp = requests.get(url, stream=True).raw
-except requests.exceptions.RequestException as e:
-    sys.exit(1)
-
-try:
-    img = Image.open(resp)
-except IOError:
-    print("Unable to open image")
-    sys.exit(1)
-img.save('temporary.jpg', 'jpeg')
-enhancer = ImageEnhance.Contrast(img)
-factor = 0.8
-im_output = enhancer.enhance(factor)
-im_output.save('temporary.jpg', 'jpeg')
 
 
-def shrek(img, delta):
+
+
+def color_1(img, delta):
+    print('Введите значения r,g,b (0-255) через enter')
+    r1 = int(input())
+    g1 = int(input())
+    b1 = int(input())
     im = Image.open(img)
     pix = im.load()
     im1 = im.copy()
@@ -33,14 +18,43 @@ def shrek(img, delta):
     for i in range(x):
         for j in range(y):
             r, g, b = pix[i, j]
-            pix[i, j] = r, g, b
-            pix1[i, j] = b, g, r
-    for i in range(x - delta):
-        for j in range(y):
-            r, g, b = pix[i + delta, j]
-            r1, g1, b1 = pix1[i, j]
-            pix[i + delta, j] = r1, g, b
-    im.save('shrek.jpeg')
+            pix[i, j] = r1, g1, b1
+    im.save('color.jpeg')
 
 
-shrek('temporary.jpg', 10)
+def color_2(img):
+    im = Image.open(img)
+    pix = im.load()
+    x, y = im.size
+    r_new, g_new, b_new = pix[x / 2, y / 2]
+    print(r_new, g_new, b_new)
+
+
+print("Для генерации цвета по заданным параметрам r,g,b введите 1")
+print("Для определения параметров r,g,b цвета по ссылке введите 2")
+flag = int(input())
+
+if flag == 1:
+    color_1('temporary.jpg', 10)
+if flag == 2:
+    print(
+        'Введите ссылку на картинку в формате .jpg (Для точного опредеения картинка должна состоять из одинаковых пикселей(быть монотонной))')
+    url = input()
+    try:
+        resp = requests.get(url, stream=True).raw
+    except requests.exceptions.RequestException as e:
+        sys.exit(1)
+
+    try:
+        img = Image.open(resp)
+    except IOError:
+        print("Unable to open image")
+        sys.exit(1)
+    img.save('temporary.jpg', 'jpeg')
+    enhancer = ImageEnhance.Contrast(img)
+    factor = 1
+    im_output = enhancer.enhance(factor)
+    im_output.save('temporary.jpg', 'jpeg')
+    color_2('temporary.jpg')
+
+
